3d3f7253954af601cfb3960ac2c7957532f563f7
haraphat01 <haraphat@gmail.com>
2023-01-03 20:42:24 +0100

    Fixed the about us page, only the CSS left

--- src/components/Section5/About.js
+++ src/components/Section5/About.js
@@ -1,26 +1,25 @@
-import React from "react";
-import avatar from "../../images/image.jpeg";
-import Calendy from "../Calendy/Calendy";
-import "./About.css";
+import React from 'react';
+import './About.css';
 
 function About() {
   return (
     <div className="about-me-container">
-      <h2>
-        Let's Book a call, <br /> So that we can discuss about your next step:
-      </h2>
+      
       <div className="about-me-content">
-        <div className="about-me-avatar">
-          <img src={avatar} alt="avatar" />
-          <p>
-            <strong>@Pencil:</strong> We are freelancer web developer, designer
-            and trainer. We educate/train/help people to get their dream job or
-            start their career as a freelancer or build thier next big webapp.
-          </p>
-        </div>
-        <div className="about-me-calendar">
-          <Calendy />
-        </div>
+        <p>
+          Pencil is dedicated to helping businesses and organizations succeed in
+          the digital world. We offer a range of services, including web
+          development, software development, and product design, to help our
+          clients achieve their goals. Our team is made up of highly skilled and
+          experienced professionals who are passionate about what they do. We
+          take pride in the quality of our work and strive to exceed our
+          clients' expectations on every project. We believe in building
+          long-term relationships with our clients and are committed to
+          providing excellent customer service. Whether you need a new website,
+          custom software, or help with product design, we have the expertise to
+          get the job done right. Thank you for considering us for your digital
+          needs. We look forward to working with you!"
+        </p>
       </div>
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -14,7 +14,7 @@ function Home() {
       <Services />
       <Portfolio />
       <Testimonial/>
-      <About />
+    
     </div>
   )
 }
